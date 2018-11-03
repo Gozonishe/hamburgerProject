e0a5cc555e3a7e27347e99fdb75a2cd88189aae8
Andrey <andrey.gozon@gmail.com>
2018-11-03 21:36:57 +0300

    cards with images

---
+++ src/components/productList/productList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ProductCard from '../productCard/productCard';
+
+class ProductList extends Component {
+    constructor(props) {
+        super(props);  
+        this.state = {
+            productsData: [],
+            count: 0,
+        };
+      }
+
+  componentDidMount() {
+    fetch('https://snowstormapi.herokuapp.com/products')
+    .then(res => res.json())
+    .then(data => this.setState({
+        count: data.count,
+        productsData: data.products,
+     }))
+  }    
+
+
+  getList() {
+    const {productsData} = this.state;
+    return productsData.map(product => <ProductCard {...product}/>)
+  }
+
+  render() {
+    return (
+        <div className='main' >
+           {this.getList()}
+        </div>
+    );
+  }
+}
+
+export default ProductList;
--- src/components/shopPage/shopPage.js
+++ src/components/shopPage/shopPage.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
+import ProductList from '../productList/productList';
 
 class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            count: '',
-
-        };
-      }
-
-    componentDidMount() {
-        fetch('https://snowstormapi.herokuapp.com/products')
-            .then(res => res.json())
-            .then(data => this.setState({
-                count: data.count,
-            }));
-      }
       
     render() {
-        console.log('render');
+
     return (
         <div className='shop_page' >
             <h1>HELLO FROM SHOP PAGE!!!</h1>
-            <h1>total count: {this.state.count}</h1>
+            <h2>Тут будут фильтры</h2>
+            <ProductList/>
         </div>
     );
   }
