c30effc380018a43e03a02e83fda1296ee16c4f2
Andrey Travin <andrey.gozon@mail.ru>
2018-11-18 15:48:04 +0300

    test from main pc

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -24,6 +24,8 @@ class MainPage extends Component {
     return (
         <div className='mainPage' >
             <h1>Snow Strorm Snowboarding Store</h1>
+            <h1>Snow Strorm Snowboarding Store</h1>
+            <h1>Snow Strorm Snowboarding Store</h1>
             <img class = 'mainBackground' src = "https://learnex.com.mx/home/wp-content/uploads/2017/12/snowb.jpg"/> 
             <Button onClick={this.handleIncrement}>Do Increment</Button>
             <Button onClick={this.handleReduce}>Do Reduce</Button>
